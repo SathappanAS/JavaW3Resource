5639e481cc3dfaa7ecf49d7cfe31aa7e66017e19
sathappan1989 <sathappanas@outlook.com>
2025-04-09 14:01:23 -0700

    sum

---
+++ src/BasicPartOne/SumofTwoNumbers.java
@@ -0,0 +1,22 @@
+package BasicPartOne;
+
+public class SumofTwoNumbers {
+	
+	public static void sum(int a,int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		   
+//		//Calculate the sum of 24 and 26
+//		byte num = 24;
+//		byte num1 = 26;
+//		
+//		int sum = num + num1;
+//		System.out.println(sum);
+		sum(10,20);
+
+	}
+
+}
