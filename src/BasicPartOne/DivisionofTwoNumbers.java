aa4e65f020702fbe4d040b89d56cc523d4defccb
sathappan1989 <sathappanas@outlook.com>
2025-04-11 21:56:06 -0700

    BasisProgram

---
+++ src/BasicPartOne/DivisionofTwoNumbers.java
@@ -0,0 +1,17 @@
+package BasicPartOne;
+
+public class DivisionofTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=50;
+		int b=3;
+		int c=a/b;
+		double d=a/b;
+		
+		System.out.println(c); //quotient 16.66
+		System.out.println(d);
+
+	}
+
+}
