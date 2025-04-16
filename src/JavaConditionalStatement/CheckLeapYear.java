f5a966dd2b1a915abb109ff03f55ab46b1819416
sathappan1989 <sathappanas@outlook.com>
2025-04-16 11:22:11 -0700

    CheckLeapYear

--- src/JavaConditionalStatement/CheckLeapYear.java
+++ src/JavaConditionalStatement/CheckLeapYear.java
@@ -1,7 +1,12 @@
 package JavaConditionalStatement;
 
 import java.util.Scanner;
-
+/*It is divisible by 4
+AND
+Not divisible by 100, 
+OR
+unless it is also divisible by 400.
+*/
 public class CheckLeapYear {
 
 	public static void main(String[] args) {
