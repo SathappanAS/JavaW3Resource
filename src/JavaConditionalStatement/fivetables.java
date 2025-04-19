73afa5651f9ea7dc99a2e0bf62a71d29bd4c7e80
sathappan1989 <sathappanas@outlook.com>
2025-04-18 22:29:50 -0700

    MultiFive

---
+++ src/JavaConditionalStatement/fivetables.java
@@ -0,0 +1,21 @@
+package JavaConditionalStatement;
+
+public class fivetables {
+// 5 * 1 = 5
+// 5 * 2 = 10
+	
+	
+// 5 * 10 = 50
+	public static void main(String[] args) {
+		
+		int number = 5;
+		int multi ;
+		for(int i=1;i<=10;i++) {
+			multi = i*number;
+			System.out.println(number + "* " + i + " " + "= " + multi);
+		}
+		
+		
+	}
+
+}
