70159dbd14cd57f8bf6fb7ab452c8d4e428624f6
sathappan1989 <sathappanas@outlook.com>
2025-04-18 22:31:42 -0700

    MultiFive

---
+++ src/JavaConditionalStatement/Vowel.java
@@ -0,0 +1,13 @@
+package JavaConditionalStatement;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+	
+		for(char ch = 'A';ch<='Z';ch++) {
+			System.out.println(ch + " " + (int)ch);
+		}
+
+	}
+
+}
