f9736b0752644c90ca0366afcb74ed2e8de4201b
sathappan1989 <sathappanas@outlook.com>
2025-04-19 08:03:27 -0700

    Fibonaccisequence

--- src/JavaConditionalStatement/Fibonaccisequence.java
+++ src/JavaConditionalStatement/Fibonaccisequence.java
@@ -1,7 +1,7 @@
 package JavaConditionalStatement;
 /**/
 public class Fibonaccisequence {
-//0 1 1 2 3 5 8 13 21 
+//0 1 1 2 3 5 8 13 21 ...
 	public static void main(String[] args) {
 		
 		int num = 0;
@@ -9,6 +9,7 @@ public class Fibonaccisequence {
 		int sum=0;
 		System.out.println(num);
 		System.out.println(num1);
+		
 		for(int i=1;i<=10;i++) {
 			sum=num+num1;
 			System.out.println(sum);
