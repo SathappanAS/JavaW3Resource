d9b0807a8fcfbef36057172f382364b6f7dd941a
sathappan1989 <sathappanas@outlook.com>
2025-04-20 22:16:01 -0700

    P6_IndexofAnArrayElement.java

--- src/JavaArrays/P6_IndexofAnArrayElement.java
+++ src/JavaArrays/P6_IndexofAnArrayElement.java
@@ -4,33 +4,70 @@ import java.util.OptionalInt;
 
 public class P6_IndexofAnArrayElement {
 
-	public static OptionalInt findIndex(int[] my_array, int t) {
+//	public static OptionalInt findIndex(int[] my_array, int t) {
+//
+//		if (my_array == null)
+//			return OptionalInt.empty();
+//		
+//		int len = my_array.length;
+//		int i = 0;
+//		
+//		while (i < len) {
+//			
+//			if (my_array[i] == t)
+//				return OptionalInt.of(i);
+//			else
+//				i++;
+//		}
+//		return OptionalInt.empty();
+//	}
+//
+//	public static void main(String[] args) {
+//		
+//		int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+//	
+//		findIndex(my_array, 25).ifPresent(index -> System.out.println("Index position of 25 is: " + index));
+//	
+//		findIndex(my_array, 877).ifPresent(index -> System.out.println("Index position of 877 is: " + index));
+//
+//		findIndex(my_array, 29).ifPresent(index -> System.out.println("Index position of 29 is: " + index));
+//	}
 
-		if (my_array == null)
-			return OptionalInt.empty();
+	public static int my_array(int a[],int num) {
 		
-		int len = my_array.length;
-		int i = 0;
-		
-		while (i < len) {
-			
-			if (my_array[i] == t)
-				return OptionalInt.of(i);
-			else
-				i++;
+		if (a == null) {
+			System.out.println("empty array");
 		}
-		return OptionalInt.empty();
+		
+		int len = a.length;
+		int i=0;
+		while(i<a.length) {
+			if(a[i]==num) {
+				System.out.println(i);
+				break;
+			}
+			else {
+				i++;
+			}
+		}
+		
+		
+		return 0;
 	}
-
+	
 	public static void main(String[] args) {
 		
-		int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+	int a[]= new int[5];
+	a[0]=10;
+	a[1]=20;
+	a[2]=30;
+	a[3]=40;
+	a[4]=50;
 	
-		findIndex(my_array, 25).ifPresent(index -> System.out.println("Index position of 25 is: " + index));
 	
-		findIndex(my_array, 877).ifPresent(index -> System.out.println("Index position of 877 is: " + index));
-
-		findIndex(my_array, 29).ifPresent(index -> System.out.println("Index position of 29 is: " + index));
-	}
-
-}
+	
+	my_array(a,10);
+	
+	
+	
+}}
