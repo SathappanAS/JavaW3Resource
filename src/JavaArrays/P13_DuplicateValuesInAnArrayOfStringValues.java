f44c0b8c2d0c79b50529886ad2daa9d0a5adc5f5
sathappan1989 <sathappanas@outlook.com>
2025-04-21 22:05:31 -0700

    P13_DuplicateValuesInAnArrayOfStringValues.java

---
+++ src/JavaArrays/P13_DuplicateValuesInAnArrayOfStringValues.java
@@ -0,0 +1,25 @@
+package JavaArrays;
+
+public class P13_DuplicateValuesInAnArrayOfStringValues {
+	/* Write a Java program to find duplicate values in an array of string values */
+	public static void main(String[] args) {
+		
+		 String[] my_array = {"bcd", "abd", "jude", "bcd", "oiu", "gzw", "oiu"};
+		 
+	        // Iterate through the elements of the string array.
+	        for (int i = 0; i < my_array.length-1; i++) {
+	            for (int j = i+1; j < my_array.length; j++) {
+	                // Check if two string elements are equal and not the same element.
+	                if ((my_array[i].equals(my_array[j])) && (i != j)) {
+	                    // If a duplicate is found, print the duplicate element.
+	                    System.out.println("Duplicate Element is : " + my_array[j]);
+	                }
+				  
+				  
+			  }
+		  }
+
+	}
+
+}
+	
