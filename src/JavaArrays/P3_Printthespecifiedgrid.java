101a4dc194aa9122fe2131cfc9983427cae49d3a
sathappan1989 <sathappanas@outlook.com>
2025-04-24 21:57:07 -0700

    P3_Printthespecifiedgrid.java

--- src/JavaArrays/P3_Printthespecifiedgrid.java
+++ src/JavaArrays/P3_Printthespecifiedgrid.java
@@ -11,7 +11,7 @@ public class P3_Printthespecifiedgrid {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 // Print each element of the array, ensuring a minimum width of 2 characters.
-                System.out.printf("%2d ", a[i][j]);
+            	System.out.print(" " + a[i][j]);
             }
             // Move to the next line to represent a new row in the output.
             System.out.println();
